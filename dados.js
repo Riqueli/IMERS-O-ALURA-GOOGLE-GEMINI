0a69220fba41461bd6eecefcb1ec0bd50a6fdcba
Riqueli Silva <96599547+Riqueli@users.noreply.github.com>
2024-09-09 21:54:39 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Raissa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos de idade, ela já coleciona títulos e inspira jovens skatistas.",
+        link: "https://pt.wikipedia.org/wiki/Raissa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com diversas medalhas em seu currículo, ela é um dos principais nomes do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judo judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com suas incríveis performances. Bicampeã olímpica, ela se tornou um símbolo de superação e determinação, inspirando jovens atletas em todo o país.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica ouro"
+    }
+];
